ac3a24ec08373b3c3b589ac66c59025c85b82c8e
annapawelec <anna.pawelec94@gmail.com>
2022-11-17 19:37:13 +0100

    add runner

--- src/main/java/pl/annapawelec/Runner.java
+++ src/main/java/pl/annapawelec/Runner.java
@@ -1,14 +1,21 @@
 package pl.annapawelec;
 
-import pl.annapawelec.builder.Water;
+
+import pl.annapawelec.project.designpatterns.strategy.Coffee;
+import pl.annapawelec.project.designpatterns.strategy.Drink;
+import pl.annapawelec.project.designpatterns.strategy.Mug;
+import pl.annapawelec.project.designpatterns.strategy.Water;
 
 public class Runner {
     public static void main(String[] args) {
-        Water water = new Water.WaterBuilder()
-                .setName("woda")
-                .setCalciumLevel(100)
-                .setFluorLevel(50)
-                .build();
-        System.out.println(water);
+//        Water water = new Water.WaterBuilder()
+//                .setName("woda")
+//                .setCalciumLevel(100)
+//                .setFluorLevel(50)
+//                .build();
+//        System.out.println(water);
+        Drink water = new Coffee();
+        Mug mug = new Mug(water);
+        mug.coffeinate();
     }
 }
